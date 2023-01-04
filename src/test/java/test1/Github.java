1d3ab4758adc56ce75c4783dc0d2242c757c5e18
DELL <DELL@DELL-PC>
2023-01-04 16:10:40 +0530

    wishes are created as java program

---
+++ src/test/java/test1/Github.java
@@ -0,0 +1,7 @@
+package test1;
+
+public class Github {
+public static void main(String[] args) {
+	System.out.println("Good morning");
+}
+}
---
+++ src/test/java/test1/Github2.java
@@ -0,0 +1,7 @@
+package test1;
+
+public class Github2 {
+public static void main(String[] args) {
+	System.out.println("hiii");
+}
+}
